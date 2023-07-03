a6ba8ed8e201ec1c8d72f21271d66803d58708dd
MichaelKwan34 <134907341+MichaelKwan34@users.noreply.github.com>
2023-07-02 18:08:34 -0700

    Update UsersController.java

--- src/main/java/com/example/demo/controllers/UsersController.java
+++ src/main/java/com/example/demo/controllers/UsersController.java
@@ -1,5 +1,30 @@
 package com.example.demo.controllers;
 
-public class UsersController {
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.model.User;
+import com.example.demo.model.UserRepository;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+@Controller
+public class UserController {
+    @Autowired
+    private UserRepository userRepo;
+    
+    @GetMapping("/signUp")
+    public String signUp(@RequestParam Map<String, String> account, HttpServletResponse response) {
+        String username = account.get("username");
+        String password = account.get("password");
+        userRepo.save(new User(username, password));
+        response.setStatus(201);
+        // add an endpoint
+        return ""; 
+    }    
 }
