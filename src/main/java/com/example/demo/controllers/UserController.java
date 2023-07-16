64dfa3eb3516bce9030fb6483baf1be33325efa3
Arthur Lam <117553934+Porsugu@users.noreply.github.com>
2023-07-16 16:36:09 -0700

    Update UserController.java, del / for endpoint

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -58,15 +58,15 @@ public class UserController {
     @GetMapping("/user/login")
     public String goSignin(@RequestParam Map<String, String> info, Model model, HttpSession session) {
        User user = (User) session.getAttribute("session_user");
-    if (user == null) {
-    return "/user/login";
-    } else {
-    model.addAttribute("user", user);
-    model.addAttribute("username", user.getUsername());
-    model.addAttribute("uid", user.getUid());
-    return "/user/userCentre";
+        if (user == null) {
+            return "user/login";
+        } else {
+            model.addAttribute("user", user);
+            model.addAttribute("username", user.getUsername());
+            model.addAttribute("uid", user.getUid());
+            return "user/userCentre";
+        }
     }
-}
 
     @PostMapping("/user/login")
     public String login(@RequestParam Map<String, String> info, Model model,
