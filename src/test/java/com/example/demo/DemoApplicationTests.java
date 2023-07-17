bf6e69270a20c3c2ef099fd44c9da079cc9b2bc3
Kathryn <ktanardy@gmail.com>
2023-07-16 19:51:29 -0700

    Modified user constructor and created test cases

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,7 +1,16 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.model.User;
+import com.example.demo.model.UserRepository;
 
 @SpringBootTest
 class DemoApplicationTests {
@@ -10,4 +19,17 @@ class DemoApplicationTests {
 	void contextLoads() {
 	}
 
+	@Autowired
+	private UserRepository userRepoTest;
+
+	@Test
+	void createAccount() {
+		User newUser = new User("testUname", "testPassword", "ilovemygroup123@gmail.com");
+		List<User> testFind = userRepoTest.findByUsernameAndPassword("testUname", "testPassword");
+		if (testFind.isEmpty()) {
+			fail();
+		} else {
+			assertTrue(true);
+		}
+	}
 }
