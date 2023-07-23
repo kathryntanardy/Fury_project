c1c0e37fff8a6a72b83cdc472c99508079f843d0
Kathryn <ktanardy@gmail.com>
2023-07-23 13:37:25 -0700

    Updated css

--- src/main/java/com/example/demo/controllers/AdminController.java
+++ src/main/java/com/example/demo/controllers/AdminController.java
@@ -34,7 +34,7 @@ public class AdminController {
     @Autowired
     private userMessageRepository userMsgRepo;
 
-    @Autowired 
+    @Autowired
     private UserRepository userRepo;
 
     @Autowired
@@ -44,40 +44,41 @@ public class AdminController {
     public String goSignin(@RequestParam Map<String, String> info, Model model, HttpSession session) {
         Admin admin = (Admin) session.getAttribute("session_user");
         if (admin == null) {
-        return "admin/login";
+            return "admin/login";
         } else {
-        model.addAttribute("user", admin);
-        model.addAttribute("username", admin.getUsername());
-        return "admin/adminCentre";
+            model.addAttribute("user", admin);
+            model.addAttribute("username", admin.getUsername());
+            return "admin/adminCentre";
+        }
     }
-}
+
     @PostMapping("/admin/login")
     public String login(@RequestParam Map<String, String> info, Model model,
             HttpServletRequest request, HttpSession session) {
         List<Admin> adminList = adminRepo.findByUsernameAndPassword(info.get("username"),
                 info.get("password"));
-        Boolean badLogin=false;
-        Boolean blankName=info.get("username").equals("");
-        Boolean blankPassword=info.get("password").equals("");
-        Boolean wrongPassword=adminRepo.findByUsername(info.get("username")).size()!=0 && adminList.size()==0;
-        Boolean noUser=adminRepo.findByUsername(info.get("username")).size()==0;
-        if(blankName){
+        Boolean badLogin = false;
+        Boolean blankName = info.get("username").equals("");
+        Boolean blankPassword = info.get("password").equals("");
+        Boolean wrongPassword = adminRepo.findByUsername(info.get("username")).size() != 0 && adminList.size() == 0;
+        Boolean noUser = adminRepo.findByUsername(info.get("username")).size() == 0;
+        if (blankName) {
             model.addAttribute("usernameAlert", "Username required");
-            badLogin=true;
+            badLogin = true;
         }
 
-        if(blankPassword){
+        if (blankPassword) {
             model.addAttribute("passwordAlert", "Password required");
-            badLogin=true;
-        } 
-        if(noUser && !blankPassword){
+            badLogin = true;
+        }
+        if (noUser && !blankPassword) {
             model.addAttribute("usernameAlert", "Admin not found");
             model.addAttribute("passwordAlert", "");
-            badLogin=true;
+            badLogin = true;
         }
-        if(!blankName && !blankPassword && wrongPassword){
+        if (!blankName && !blankPassword && wrongPassword) {
             model.addAttribute("passwordAlert", "Wrong password");
-            badLogin=true;
+            badLogin = true;
         }
         if (badLogin) {
             return "admin/login";
@@ -90,29 +91,31 @@ public class AdminController {
         }
     }
 
-    //by 4
-    //handle btn clicked
+    // by 4
+    // handle btn clicked
     @PostMapping("/admin/buttonClicked")
-    public String handleButtonClick(@RequestParam("buttonValue") String buttonValue,@RequestParam Map<String, String> info, Model model,HttpServletRequest request, HttpSession session) {
+    public String handleButtonClick(@RequestParam("buttonValue") String buttonValue,
+            @RequestParam Map<String, String> info, Model model, HttpServletRequest request, HttpSession session) {
         if (buttonValue.equals("Inbox")) {
-            List<userMessage> inbox=userMsgRepo.findAll();
+            List<userMessage> inbox = userMsgRepo.findAll();
             model.addAttribute("inbox", inbox);
             return "admin/adminInbox";
-        } 
-        if(buttonValue.equals("Reply")){
-            userMessage msg=userMsgRepo.findByMid(Integer.parseInt(info.get("mid"))).get(0);
+        }
+        if (buttonValue.equals("Reply")) {
+            userMessage msg = userMsgRepo.findByMid(Integer.parseInt(info.get("mid"))).get(0);
             model.addAttribute("mid", info.get("mid"));
             model.addAttribute("msg", msg);
-            int fromUid=userMsgRepo.findByMid(Integer.parseInt(info.get("mid"))).get(0).getFromUid();
-            String fromUser=userRepo.findByUid(fromUid).get(0).getUsername();
-            model.addAttribute("subject", "Re: "+msg.getSubject());
-            model.addAttribute("content","Dear "+fromUser+",\n\n"+"Best,\n"+"Team Fury\n\n"+"[Quote: \n"+msg.getContent()+"\n]");
+            int fromUid = userMsgRepo.findByMid(Integer.parseInt(info.get("mid"))).get(0).getFromUid();
+            String fromUser = userRepo.findByUid(fromUid).get(0).getUsername();
+            model.addAttribute("subject", "Re: " + msg.getSubject());
+            model.addAttribute("content", "Dear " + fromUser + ",\n\n" + "Best,\n" + "Team Fury\n\n" + "[Quote: \n"
+                    + msg.getContent() + "\n]");
             model.addAttribute("fromUser", fromUser);
             return "admin/reply";
         }
-        if(buttonValue.equals("playerDatabase")){
-            List<User> userList=userRepo.findAll();
-            model.addAttribute("userNum",userList.size());
+        if (buttonValue.equals("playerDatabase")) {
+            List<User> userList = userRepo.findAll();
+            model.addAttribute("userNum", userList.size());
             model.addAttribute("userList", userList);
             return "admin/playerDatabase";
         }
@@ -120,42 +123,41 @@ public class AdminController {
             request.getSession().invalidate();
             return "admin/login";
         }
-        if(buttonValue.equals("trend")){
-            int currentYear=LocalDate.now().getYear();
+        if (buttonValue.equals("trend")) {
+            int currentYear = LocalDate.now().getYear();
             model.addAttribute("year", currentYear);
             model.addAttribute("data", activeUserInYear(currentYear));
-            model.addAttribute("type", "Actice Users");
-            model.addAttribute("otherType", "Commulative users");
+            model.addAttribute("type", "Active Users");
+            model.addAttribute("otherType", "Cumulative users");
             return "admin/trend";
         }
-        if(buttonValue.equals("announcement")){
+        if (buttonValue.equals("announcement")) {
             return "admin/announcement";
         }
         return "admin/adminCentre";
     }
-    
+
     @PostMapping("/admin/announcement")
-    public String postAnnouncement(@RequestParam Map<String, String> message, Model model){
-        boolean goodMsg=true;
-        String feedback="";
-        if(message.get("subject").equals("")){
-            goodMsg=false;
-            feedback="subject is";
+    public String postAnnouncement(@RequestParam Map<String, String> message, Model model) {
+        boolean goodMsg = true;
+        String feedback = "";
+        if (message.get("subject").equals("")) {
+            goodMsg = false;
+            feedback = "subject is";
             System.out.println("bad subject");
         }
-        if(message.get("content").equals("")){
-            goodMsg=false;
-            if(feedback.equals("")){
-                feedback="content is";
-            }
-            else{
-                feedback="subject and content are";
+        if (message.get("content").equals("")) {
+            goodMsg = false;
+            if (feedback.equals("")) {
+                feedback = "content is";
+            } else {
+                feedback = "subject and content are";
             }
             System.out.println("bad content");
         }
-        if(!goodMsg){
-            feedback+=" null";
-            feedback="Error: "+feedback;
+        if (!goodMsg) {
+            feedback += " null";
+            feedback = "Error: " + feedback;
             System.out.println(feedback);
             model.addAttribute("feedback", feedback);
             model.addAttribute("mid", message.get("mid"));
@@ -165,42 +167,40 @@ public class AdminController {
             return "admin/sendResult";
         }
 
-        //None of the rows are null
+        // None of the rows are null
         System.out.println("Good msg");
-        adminMsgRepo.save(new adminMessage("Announcement",0, message.get("subject"), message.get("content"),LocalDate.now(),"N"));
+        adminMsgRepo.save(new adminMessage("Announcement", 0, message.get("subject"), message.get("content"),
+                LocalDate.now(), "N"));
         model.addAttribute("feedback", "Message sent!!");
         model.addAttribute("btnValue", "back");
-            model.addAttribute("btnText", "Back to admin centre");
+        model.addAttribute("btnText", "Back to admin centre");
         return "admin/sendResult";
     }
 
     @PostMapping("/admin/trend")
-    public String switchYear(@RequestParam("buttonValue") String buttonValue,@RequestParam Map<String, String> info, Model model){
-        int year=Integer.parseInt(info.get("year"));
-        String type=info.get("type");
-        String otherType=info.get("otherType");
-        String data="";
-        if(buttonValue.equals("switchType")){
-            otherType=info.get("type");
-            if(type.equals("Actice Users")){
-                type="Commulative users";
+    public String switchYear(@RequestParam("buttonValue") String buttonValue, @RequestParam Map<String, String> info,
+            Model model) {
+        int year = Integer.parseInt(info.get("year"));
+        String type = info.get("type");
+        String otherType = info.get("otherType");
+        String data = "";
+        if (buttonValue.equals("switchType")) {
+            otherType = info.get("type");
+            if (type.equals("Active Users")) {
+                type = "Cumulative users";
+            } else {
+                type = "Active Users";
             }
-            else{
-                type="Actice Users";
-            }
-        }
-        else if(buttonValue.equals("nextYear")){
+        } else if (buttonValue.equals("nextYear")) {
             year++;
-        }
-        else if (buttonValue.equals("previousYear")) {
+        } else if (buttonValue.equals("previousYear")) {
             year--;
         }
-        
-        if(type.equals("Actice Users")){
-            data=activeUserInYear(year);
-        }
-        else{
-            data=commulativUserInYear(year);
+
+        if (type.equals("Active Users")) {
+            data = activeUserInYear(year);
+        } else {
+            data = commulativUserInYear(year);
         }
         model.addAttribute("type", type);
         model.addAttribute("otherType", otherType);
@@ -209,87 +209,84 @@ public class AdminController {
         return "admin/trend";
     }
 
-    private String activeUserInYear(int year){
+    private String activeUserInYear(int year) {
         List<User> allUsers = userRepo.findAll();
         allUsers.sort(Comparator.comparing(User::getLastLoginDate));
-        int[] dataArr=new int[12];
-        String data="";
-        for(User u :allUsers){
-            LocalDate lastLogin=u.getLastLoginDate();
-            if(lastLogin.getYear()==year){
-                int month=lastLogin.getMonthValue();
-                dataArr[month-1]++;
+        int[] dataArr = new int[12];
+        String data = "";
+        for (User u : allUsers) {
+            LocalDate lastLogin = u.getLastLoginDate();
+            if (lastLogin.getYear() == year) {
+                int month = lastLogin.getMonthValue();
+                dataArr[month - 1]++;
             }
         }
-        for(int i:dataArr){
-            data+=i+",";
+        for (int i : dataArr) {
+            data += i + ",";
         }
-        return data.substring(0, data.length()-1);
+        return data.substring(0, data.length() - 1);
     }
 
-    private String commulativUserInYear(int year){
+    private String commulativUserInYear(int year) {
         List<User> allUsers = userRepo.findAll();
         allUsers.sort(Comparator.comparing(User::getLastLoginDate));
-        int[] dataArr=new int[12];
-        String data="";
-        for(User u :allUsers){
-            LocalDate registerDate=u.getRegisterDate();
-            if(registerDate.getYear()<year){
-                for(int i=0;i<12;i++){
+        int[] dataArr = new int[12];
+        String data = "";
+        for (User u : allUsers) {
+            LocalDate registerDate = u.getRegisterDate();
+            if (registerDate.getYear() < year) {
+                for (int i = 0; i < 12; i++) {
+                    dataArr[i]++;
+                }
+            } else if (registerDate.getYear() == year) {
+                int month = registerDate.getMonthValue();
+                for (int i = month - 1; i < 12; i++) {
                     dataArr[i]++;
                 }
             }
-            else if(registerDate.getYear()==year){
-                int month=registerDate.getMonthValue();
-                for(int i=month-1;i<12;i++){
-                    dataArr[i]++;
-                }
-            }
 
         }
-        for(int i:dataArr){
-            data+=i+",";
+        for (int i : dataArr) {
+            data += i + ",";
         }
-        return data.substring(0, data.length()-1);
+        return data.substring(0, data.length() - 1);
     }
-
 
     @PostMapping("/admin/readInbox")
-    public String readUserInbox(@RequestParam("buttonValue") String buttonValue,@RequestParam Map<String, String> info, Model model) {
-        List<userMessage> msg =userMsgRepo.findByMid(Integer.parseInt(buttonValue));
-        model.addAttribute("msg",msg.get(0));
-        String fromUser=userRepo.findByUid(msg.get(0).getFromUid()).get(0).getUsername();
+    public String readUserInbox(@RequestParam("buttonValue") String buttonValue, @RequestParam Map<String, String> info,
+            Model model) {
+        List<userMessage> msg = userMsgRepo.findByMid(Integer.parseInt(buttonValue));
+        model.addAttribute("msg", msg.get(0));
+        String fromUser = userRepo.findByUid(msg.get(0).getFromUid()).get(0).getUsername();
         model.addAttribute("fromUser", fromUser);
         return "admin/adminReadInbox";
     }
 
-    
-    //by 4
-    //add msg to database
+    // by 4
+    // add msg to database
     @PostMapping("/admin/reply")
     public String sendMsg(@RequestParam Map<String, String> message, Model model) {
-        //return "/admin/adminCentre";
-        //Check if any of the input is/are null
-        boolean goodMsg=true;
-        String feedback="";
-        if(message.get("subject").equals("")){
-            goodMsg=false;
-            feedback="subject is";
+        // return "/admin/adminCentre";
+        // Check if any of the input is/are null
+        boolean goodMsg = true;
+        String feedback = "";
+        if (message.get("subject").equals("")) {
+            goodMsg = false;
+            feedback = "subject is";
             System.out.println("bad subject");
         }
-        if(message.get("content").equals("") || message.get("content").equals(message.get("tempContent"))){
-            goodMsg=false;
-            if(feedback.equals("")){
-                feedback="content is";
-            }
-            else{
-                feedback="subject and content are";
+        if (message.get("content").equals("") || message.get("content").equals(message.get("tempContent"))) {
+            goodMsg = false;
+            if (feedback.equals("")) {
+                feedback = "content is";
+            } else {
+                feedback = "subject and content are";
             }
             System.out.println("bad content");
         }
-        if(!goodMsg){
-            feedback+=" null";
-            feedback="Error: "+feedback;
+        if (!goodMsg) {
+            feedback += " null";
+            feedback = "Error: " + feedback;
             System.out.println(feedback);
             model.addAttribute("feedback", feedback);
             model.addAttribute("mid", message.get("mid"));
@@ -299,26 +296,27 @@ public class AdminController {
             return "admin/sendResult";
         }
 
-        //None of the rows are null
+        // None of the rows are null
         System.out.println("Good msg");
-        userMessage msgFromUser=userMsgRepo.findByMid(Integer.parseInt(message.get("mid"))).get(0);
-        int toUid=msgFromUser.getFromUid();
+        userMessage msgFromUser = userMsgRepo.findByMid(Integer.parseInt(message.get("mid"))).get(0);
+        int toUid = msgFromUser.getFromUid();
         msgFromUser.setSolved("Y");
         userMsgRepo.save(msgFromUser);
-        adminMsgRepo.save(new adminMessage("Reply",toUid, message.get("subject"), message.get("content"),LocalDate.now(),"N"));
+        adminMsgRepo.save(
+                new adminMessage("Reply", toUid, message.get("subject"), message.get("content"), LocalDate.now(), "N"));
         model.addAttribute("feedback", "Message sent!!");
         model.addAttribute("btnValue", "back");
-            model.addAttribute("btnText", "Back to admin centre");
+        model.addAttribute("btnText", "Back to admin centre");
         return "admin/sendResult";
     }
 
     @PostMapping("/admin/handleUser")
     public String handleUser(HttpServletRequest request, Model model) {
         String[] selectedUsers = request.getParameterValues("userRow");
-        String uidList="";
+        String uidList = "";
         if (selectedUsers != null) {
             for (String uid : selectedUsers) {
-              uidList+=uid+",";
+                uidList += uid + ",";
             }
         }
         System.out.println(uidList);
