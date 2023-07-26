0f19f7ba33c4f8add16b431cb3f528bb23184db4
Kathryn <ktanardy@gmail.com>
2023-07-26 00:09:22 -0700

    Added email sender service

--- src/main/java/com/example/demo/model/UserRepository.java
+++ src/main/java/com/example/demo/model/UserRepository.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User,String> {
+public interface UserRepository extends JpaRepository<User, String> {
     List<User> findByUsernameAndPassword(String username, String password);
+
     List<User> findByUsername(String username);
+
     List<User> findByUid(int uid);
+
+    List<User> findByEmailAddress(String emailAddress);
 }
---
+++ src/main/java/com/example/demo/service/EmailSenderService.java
@@ -0,0 +1,25 @@
+package com.example.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailSenderService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendEmail(String to, String body, String subject) {
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setFrom("ilovemygroup123@gmail.com");
+        message.setTo(to);
+        message.setText(body);
+        message.setSubject(subject);
+
+        mailSender.send(message);
+        System.out.println("Mail sent");
+    }
+}
